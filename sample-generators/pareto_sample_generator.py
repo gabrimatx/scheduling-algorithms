cea2a367d42c1d6948255a3b93d63cb5d283f8ec
Gabriele Matiddi <gabrimat1@protonmail.ch>
2024-03-24 16:57:43 +0100

    Repositioned data cleaners for using other cluster traces

--- sample-generators/pareto_sample_generator.py
+++ sample-generators/pareto_sample_generator.py
@@ -1,13 +1,9 @@
 import numpy as np
 
-# Define the shape parameter (alpha) of the Pareto distribution
 alpha = 1.1
 
-# Define the size of the sample you want to generate
 sample_size = 3 * (10 ** 6)
 
-# Generate samples from the Pareto distribution
 pareto_samples = np.random.pareto(alpha, sample_size)
 
-# Print the first 10 samples
 print(np.mean(pareto_samples))
