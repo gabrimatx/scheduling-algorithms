30435194b083723aba7670f37378644728c1d492
Gabriele Matiddi <gabrimat1@protonmail.ch>
2024-03-25 16:22:47 +0100

    Added pareto augment to median oracle

--- completion-time-algorithms/oracles.py
+++ completion-time-algorithms/oracles.py
@@ -15,7 +15,53 @@ class PerfectOracle:
     def getJobPrediction(self, job: Job) -> None:
         return job.real_duration
 
+class ParetoOracle:
+    def __init__(self):
+        self.shape, self.loc, self.scale = 1.1, 0, 1
+    
+    def computePredictions(self, JobSet: list):
+        lengths = [job.real_duration for job in JobSet]
+        self.shape, self.loc, self.scale = pareto.fit(lengths)
+    
+    def getJobPrediction(self) -> float:
+        return (self.shape * self.scale) / (self.shape - 1)
+    
+class AugmentedMedianOracle:
+    def __init__(self):
+        self.jobMedians = {}
+        self.totalMedian = 0
+        self.expected_pareto = 1
 
+    def getJobPrediction(self, job: Job) -> float:
+        return self.jobMedians.get(job.id, self.expected_pareto)
+    
+    def median(self, lst):
+        sorted_lst = sorted(lst)
+        n = len(sorted_lst)
+        if n % 2 == 0:
+            mid = n // 2
+            return (sorted_lst[mid - 1] + sorted_lst[mid]) / 2
+        else:
+            return sorted_lst[n // 2]
+
+    def computePredictions(self, JobSet: list) -> None:
+        occurrences_per_id = {}
+        total_occurrences = []
+        for job in JobSet:
+            if job.id in occurrences_per_id:
+                occurrences_per_id[job.id].append(job.real_duration)
+            else:
+                occurrences_per_id[job.id] = [job.real_duration]
+
+            total_occurrences.append(job.real_duration)
+            
+        for k,v in occurrences_per_id.items():
+            self.jobMedians[k] = self.median(v)
+        self.totalMedian = self.median(total_occurrences)
+        myPareto = ParetoOracle()
+        myPareto.computePredictions(JobSet)
+        self.expected_pareto = myPareto.getJobPrediction()
+    
 class JobMeanOracle:
     def __init__(self):
         self.jobTotals = {}
@@ -45,10 +91,9 @@ class JobMedianOracle:
     def __init__(self):
         self.jobMedians = {}
         self.totalMedian = 0
-        self.myPareto = 1.1
 
     def getJobPrediction(self, job: Job) -> float:
-        return self.jobMedians.get(job.id, self.totalMedian * np.random.pareto(a = self.myPareto, size = 1)[0])
+        return self.jobMedians.get(job.id, self.totalMedian)
     
     def median(self, lst):
         sorted_lst = sorted(lst)
@@ -72,8 +117,6 @@ class JobMedianOracle:
             
         for k,v in occurrences_per_id.items():
             self.jobMedians[k] = self.median(v)
-        if total_occurrences:
-            self.myPareto = pareto.fit(total_occurrences)[0]
         self.totalMedian = self.median(total_occurrences)
                 
 
@@ -88,3 +131,11 @@ class GaussianPerturbationOracle:
     def getJobPrediction(self, Job):
         return Job.real_duration + np.random.normal(self.mean, self.std_dev, 1)[0]
 
+if __name__ == "__main__":
+    a = AugmentedMedianOracle()
+    a.computePredictions([Job(0, 0, i + 200) for i in (np.random.pareto(a = 17, size = 100000) * 2000)])
+    print(a.getJobPrediction(Job(1, 0, 0)))
+    print(a.getJobPrediction(Job(0, 0, 0)))
+    print(a.getJobPrediction(Job(1, 0, 0)))
+    print(a.getJobPrediction(Job(0, 0, 0)))
+    print(a.getJobPrediction(Job(2, 0, 0)))
